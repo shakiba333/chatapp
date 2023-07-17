90aa04e76ca4047fe0354baea0e36dbf6dcc31b5
shakiba333 <shakibaabdulrashid@gmail.com>
2023-07-17 17:28:11 -0400

    Added chat room

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
+    path('users/', views.user_list, name='user_list'),
+    path('chat_room/<str:other_username>/', views.chat_room, name='chat_room'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
