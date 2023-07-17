80e497a26908b1dc08483e9d752134e1c134f7c6
faithnicoletti <faithalicia22@gmail.com>
2023-07-17 18:21:08 -0400

    register models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import ChatRoom, Profile
 # Register your models here.
+
+admin.site.register(Profile)
+
+admin.site.register(ChatRoom)
