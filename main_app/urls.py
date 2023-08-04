b0ff60844c044e2f9371c1c11dead9c4a87571d0
shakiba333 <shakibaabdulrashid@gmail.com>
2023-08-04 18:04:31 -0400

    usercreationform

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
